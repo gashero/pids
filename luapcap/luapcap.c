8d94e08384b7c8eba2b67805da03aa9dc1368d02
harry.python <harry.python@06cef6ae-744e-0410-93a5-7f8898e9d870>
2008-06-11 04:42:30 +0000

    增加了luapcap目录，其实内含3个lua模块，分别包括lua对libpcap/libnet/protocol的支持模块。

---
+++ luapcap/luapcap.c
@@ -0,0 +1,12 @@
+/*
+ * File: luapcap.c
+ * Date: 2008-06-11
+ * Author: gashero
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+#include <pcap.h>
