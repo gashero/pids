9546b35dc0b71500b4e3c288e7dd0da48b54a019
harry.python <harry.python@06cef6ae-744e-0410-93a5-7f8898e9d870>
2009-11-09 02:00:50 +0000

    Add wormhome project, make a useful VPN program.

---
+++ wormhole/pyinject/setup.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+# File: setup.py
+# Date: 2009-11-09
+# Author: gashero
+
+"""
+PyInject library install script.
+"""
+
+from distutils.core import setup
+
+setup(
+        name='pyinject',
+        version='0.3',
+        description='Python Inject library',
+        author='gashero',
+        author_email='harry.python@gmail.com',
+        packages=['pyinject',],
+        )
